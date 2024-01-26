20a1350564f6e4614f18b0c30baec3096313a147
drianodev <adrianomendes661@gmail.com>
2024-01-26 16:04:40 -0300

    first commit

---
+++ src/main/java/br/com/drianodev/consumidorestoque/consumer/EstoqueConsumer.java
@@ -0,0 +1,27 @@
+package br.com.drianodev.consumidorestoque.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import constants.RabbitMQConstants;
+import dto.EstoqueDTO;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EstoqueConsumer {
+
+    @RabbitListener(queues = RabbitMQConstants.FILA_ESTOQUE)
+    private void consumidor(String mensagem) throws JsonProcessingException {
+
+        System.out.println("Mensagem recebida: " + mensagem);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        EstoqueDTO estoqueDTO = objectMapper.readValue(mensagem, EstoqueDTO.class);
+
+        System.out.println(estoqueDTO.codigoProduto);
+        System.out.println(estoqueDTO.quantidade);
+        System.out.println("--------------------------------------------");
+    }
+
+}
